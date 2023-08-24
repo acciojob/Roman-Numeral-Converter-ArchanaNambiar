cf06cece4209daf067379353c732b54fe06d700c
ArchanaNambiar <64783368+ArchanaNambiar@users.noreply.github.com>
2023-08-25 00:54:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -10,6 +10,33 @@ function convertToRoman(num) {
     };
 
   //your code here
+	let roman = '';
+
+  for (let i = 0; i < Object.keys(obj).length; i++) {
+    const symbol = obj[i][0];
+    const value = obj[i][1];
+
+    if (value <= num) {
+      const count = Math.floor(num / value);
+      roman += symbol.repeat(count);
+      num -= count * value;
+    }
+
+    // Check for cases like IV, IX, XL, XC, CD, CM
+    if (i % 2 === 0) {
+      const nextSymbol = obj[i - 2][0];
+      const nextValue = obj[i - 2][1];
+      const diff = nextValue - value;
+
+      if (num >= diff) {
+        roman += symbol + nextSymbol;
+        num -= diff;
+      }
+    }
+  }
+
+  return roman;
+
 
 }
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
