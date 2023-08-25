16c5585acb0dea0c20b3cc5196b38614b6a5022c
ArchanaNambiar <64783368+ArchanaNambiar@users.noreply.github.com>
2023-08-25 22:01:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,34 +1,45 @@
-function intToRoman(num) {
-  if (num <= 0 || num >= 4000) {
-    return 'Invalid input';
-  }
+function convertToRoman(num) {
+  	const obj = {
+      0:['M',1000], 
+      1:['D', 500], 
+      2:['C', 100], 
+      3:['L', 50], 
+      4:['X', 10], 
+      5:['V', 5], 
+      6:['I', 1]
+    };
 
-  const values = [
-    1000, 900, 500, 400,
-    100, 90, 50, 40,
-    10, 9, 5, 4, 1
-  ];
+  //your code here
+	let roman = '';
 
-  const symbols = [
-    'M', 'CM', 'D', 'CD',
-    'C', 'XC', 'L', 'XL',
-    'X', 'IX', 'V', 'IV',
-    'I'
-  ];
+  for (let i = 0; i < 7; i++) {
+    const symbol = obj[i][0];
+    const value = obj[i][1];
+    const count = Math.floor(num / value);
 
-  let result = '';
-
-  for (let i = 0; i < values.length; i++) {
-    while (num >= values[i]) {
-      result += symbols[i];
-      num -= values[i];
+    if (count > 0) {
+      if (symbol === 'M') {
+        roman += symbol.repeat(count);
+      } else if (count === 4 && i > 0) {
+        roman += obj[i][0] + obj[i - 1][0];
+      } else if (count === 4) {
+        roman += obj[i][0] + obj[i - 2][0];
+      } else {
+        roman += symbol.repeat(count);
+      }
+      num %= value;
     }
   }
 
-  return result;
-}
+  return roman;
 
-// Example usage
-// const number = 1984;
-// const romanNumeral = intToRoman(number);
-// console.log(romanNumeral); // Output: 'MCMLXXXIV'
+}
+// You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
+
+// console.log(convertToRoman(36));
+
+
+
+
+// do not edit below this line
+module.exports = convertToRoman
